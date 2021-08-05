72287063e0591a82104bfca2ba4e67883fc7b21e
Cecília <60898302+ninth-in@users.noreply.github.com>
2021-08-05 18:44:57 -0300

    re up att 05/08

--- Assets/4-Jojin/scripts/GameManager.cs
+++ Assets/4-Jojin/scripts/GameManager.cs
@@ -5,17 +5,27 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public AudioSource music;
-    public bool startPlaying; 
+    public static GameManager instance;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        instance = this;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void NoteHit()
+    {
+        Debug.Log("Hit on time");
+    }
+
+    public void NoteMissed()
+    {
+        Debug.Log("Missed");
     }
 }
--- Assets/4-Jojin/scripts/Notas.cs
+++ Assets/4-Jojin/scripts/Notas.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Notas : MonoBehaviour
 {
-    public bool apertado;
+
 
     // Start is called before the first frame update
     void Start()
@@ -17,20 +17,5 @@ public class Notas : MonoBehaviour
         
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if(tag == "GameController")
-        {
-            apertado = true;
-        }
-    }
-
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (tag == "GameController")
-        {
-            apertado = false;
-        }
-    }
 
 }
