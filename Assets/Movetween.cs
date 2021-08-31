446c783f05cb596ac159aebfb41a397090ba9b10
Cecília <60898302+ninth-in@users.noreply.github.com>
2021-08-31 16:05:16 -0300

    att 31/08

--- Assets/Movetween.cs
+++ Assets/Movetween.cs
@@ -7,7 +7,7 @@ public class Movetween : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        LeanTween.moveY(gameObject, 0.5f, 4).setLoopPingPong();
+        LeanTween.moveY(gameObject, 0.5f, 3).setLoopPingPong();
     }
 
     // Update is called once per frame
